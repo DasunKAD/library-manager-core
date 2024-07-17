ed2358d5576897b073704a1c7e7991602fd10901
Dasun <dasun.m@aventude.com>
2024-07-17 23:53:10 +0530

    initial project created

---
+++ src/main/java/com/dasun/library_manager_core/LibraryManagerCoreApplication.java
@@ -0,0 +1,13 @@
+package com.dasun.library_manager_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagerCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagerCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dasun/library_manager_core/LibraryManagerCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dasun.library_manager_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagerCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
